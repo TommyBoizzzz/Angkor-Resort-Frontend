1df2325a43f2bf345376f8e154bc8c371c6f5ea8
TommyBoizzzz <hengchansela168@gmail.com>
2026-06-05 11:22:08 +0700

    Feature/Auth
    Create login and register done
    with service of auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Home from "./pages/Home";
-import RoomDetail from "./pages/RoomDetail";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import Dashboard from "./pages/dashboard/Dashboard";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/room/:id" element={<RoomDetail />} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
