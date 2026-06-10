c4838f8720f41814f01b9ca55d285a2aaaf9a59b
TommyBoizzzz <hengchansela168@gmail.com>
2026-06-11 00:13:33 +0700

    feature/booking&roomdetail : create file with route

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Register from "./pages/auth/Register";
 import Dashboard from "./pages/dashboard/Dashboard";
 import AdminHome from "./pages/admin/home/home";
 import ProfileSetting from "./pages/settings/profile"
+import Booking from "./pages/booking/booking"
+import RoomDetail from "./pages/rooms/roomdetail"
 
 function App() {
   return (
@@ -16,6 +18,8 @@ function App() {
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/admin" element={<AdminHome />} />
         <Route path="/profile" element={<ProfileSetting />} />
+        <Route path="/booking" element={<Booking />} />
+        <Route path="/roomdetail" element={<RoomDetail />} />
 
         {/* hello world */}
       </Routes>
---
+++ src/pages/rooms/roomdetail.jsx
@@ -0,0 +1,10 @@
+function RoomDetail() {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <p>Welcome Room Detail</p>
+    </div>
+  );
+}
+
+export default RoomDetail;
