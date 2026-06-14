87d83eaaff2b832512dd8dc8d3d3995d57f7bb58
DavinRamsay <hengchansela168@gmail.com>
2026-06-14 12:34:13 +0700

    feature/deploy : update url api for deploy to server

--- src/config/global.js
+++ src/config/global.js
@@ -1,7 +1,7 @@
 // { Server Config url to push to production }
-// const BASE_URL = "https://angkor-resort-backend-production.up.railway.app/api";
+const BASE_URL = "https://angkor-resort-backend-production.up.railway.app/api";
 
 // { Local Config url to push to local database }
-const BASE_URL = "http://localhost:8080/api";
+// const BASE_URL = "http://localhost:8080/api";
 
 export default BASE_URL;
