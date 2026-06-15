f745a6cfc6ab8397522ad735ae2ee89bbed6b130
DavinRamsay <hengchansela168@gmail.com>
2026-06-15 12:05:16 +0700

    feature/service : update user profile service and route for room detail

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,6 +1,7 @@
 import axios from "axios";
+import BASE_URL from "../config/global";
 
-const API_URL = "http://localhost:8080/api/users";
+const API_URL = `${BASE_URL}/users`;
 
 const UserService = {
   // GET ALL USERS
